a65553058ecc4d1fd8340bf0c22b6321891c681c
LAPTOP-JC0100HH\Chawillfer <chawillfer@gmail.com>
2022-07-14 21:04:07 -0400

    Cambio #1

--- ProyectoFinalControlGastos/Login.Designer.cs
+++ ProyectoFinalControlGastos/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProyectoFinalControlGastos
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
---
+++ ProyectoFinalControlGastos/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalControlGastos.Models
+{
+    internal class Class1
+    {
+        public string TypeTrans { get; set; }
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime DateOfTrans { get; set; }
+        public string Concept { get; set; }
+        public string Reference { get; set; }
+        public float Amount { get; set; }
+        public string Moneda { get; set; }
+    }
+}
