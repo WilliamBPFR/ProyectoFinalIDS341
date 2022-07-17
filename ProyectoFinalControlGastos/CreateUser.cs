401bb32df85a7890ec85b33d415378b363aa3fa9
LAPTOP-JC0100HH\Chawillfer <chawillfer@gmail.com>
2022-07-16 22:09:24 -0400

    Cambio #1 Create User

--- ProyectoFinalControlGastos/CreateUser.cs
+++ ProyectoFinalControlGastos/CreateUser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ProyectoFinalControlGastos.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -46,7 +48,33 @@ namespace ProyectoFinalControlGastos
 
         private void button1_Click(object sender, EventArgs e)
         {
+            SaveRecord();
+        }
 
+        private void SaveRecord()
+        {
+            var json = string.Empty;
+            var pathFile = $"{AppDomain.CurrentDomain.BaseDirectory}users.json";
+            var usersList = new List<Users>();
+            var user = new Users();
+
+            if (File.Exists(pathFile)) {
+                
+                json = File.ReadAllText(pathFile);
+                usersList = JsonConvert.DeserializeObject<List<Users>>(json);
+            }
+
+            user = new Users
+            {
+                Id = 1100000 + (usersList.Count),
+                Email = textBoxEmail.Text,
+                Name = textBoxName.Text,
+                LastName = textBoxLastName.Text,
+                NickName = textBoxNickName.Text,
+                Ocupation = textBoxOcupation.Text,
+                password = textBoxPassword.Text,
+                Monedas = comboBoxCoin.Text
+            };
         }
     }
 }
